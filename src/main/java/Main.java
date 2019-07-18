d0809dac730482fba80b503c62e803582d977dff
Slavkowski <szweds@poczta.onet.pl>
2019-07-18 10:52:38 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        SCD30Impl scd30 = new SCD30Impl();
+        scd30.test();
+
+    }
+}
---
+++ src/main/java/SCD30Impl.java
@@ -0,0 +1,5 @@
+public class SCD30Impl implements SCD30 {
+    public void test(){
+        System.out.println(SCD30_I2C_ADDRESS);
+    }
+}
